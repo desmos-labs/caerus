282b6dc978b559db70f22b948116084b3e842eb2
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-08-16 12:44:15 -0500

    build: add app account to chain spawn script

--- scripts/spawn_test_chain.sh
+++ scripts/spawn_test_chain.sh
@@ -5,14 +5,19 @@ DESMOS_HOME="$SCRIPT_DIR/.desmos"
 KEYRING_PASS=pass1234
 
 # APIs account information
-APIS_ACCOUNT=bondscape-apis
+APIS_ACCOUNT=caerus-apis
 APIS_ACCOUNT_ADDRESS=desmos1nm6kh6jwqmsezwtnmgdd4w4tzyk9f8gvqu5en0
 APIS_ACCOUNT_MNEMONIC="hour harbor fame unaware bunker junk garment decrease federal vicious island smile warrior fame right suit portion skate analyst multiply magnet medal fresh sweet"
 
 # User information
-USER_ACCOUNT=bondscape-user
+USER_ACCOUNT=caerus-user
 USER_ACCOUNT_ADDRESS=desmos1c7ms9zhtgwmv5jy6ztj2vq0jj67zenw3gdl2gr
 USER_ACCOUNT_MNEMONIC="shine velvet envelope baby bicycle slot betray water mask cake rule useless absorb lens enable fork lake make depart slogan kiwi iron decorate relief"
+
+# App information
+APP_ACCOUNT=caerus-app
+APP_ACCOUNT_ADDRESS=desmos1npauelfxdyqktexm8dpcl6s93cu4fmnv84xmrw
+APP_ACCOUNT_MNEMONIC="goose kite term stamp swallow agree fruit culture cart decorate fame start decrease piece random wheel chuckle course oyster enforce jeans attract inquiry beyond"
 
 # Background flags, will be set to true if the user pass the -b argument.
 BACKGROUND=false
@@ -38,8 +43,10 @@ mv "$DESMOS_HOME/config/genesis-patched.json" "$DESMOS_HOME/config/genesis.json"
 
 (echo "$APIS_ACCOUNT_MNEMONIC"; echo $KEYRING_PASS; echo $KEYRING_PASS) | desmos keys add "$APIS_ACCOUNT" --recover --keyring-backend=file
 (echo "$USER_ACCOUNT_MNEMONIC"; echo $KEYRING_PASS; echo $KEYRING_PASS) | desmos keys add "$USER_ACCOUNT" --recover --keyring-backend=file
+(echo "$APP_ACCOUNT_MNEMONIC"; echo $KEYRING_PASS; echo $KEYRING_PASS) | desmos keys add "$APP_ACCOUNT" --recover --keyring-backend=file
 echo $KEYRING_PASS | desmos add-genesis-account $APIS_ACCOUNT 200000000000000stake --keyring-backend=file
 echo $KEYRING_PASS | desmos add-genesis-account $USER_ACCOUNT 200000000000000stake --keyring-backend=file
+echo $KEYRING_PASS | desmos add-genesis-account $APP_ACCOUNT 200000000000000stake --keyring-backend=file
 echo $KEYRING_PASS | desmos gentx $APIS_ACCOUNT 100000000000stake --amount 100000000000stake --chain-id=testchain --keyring-backend=file
 desmos collect-gentxs
 
